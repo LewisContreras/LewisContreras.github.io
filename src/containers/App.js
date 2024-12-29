55b2b2831f2d57f9aeabb3bc87ad34ab058d94e8
Lewis Contreras <legiconba4@gmail.com>
2024-12-28 22:03:47 -0500

    Add provider

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,23 @@
-import { ChakraProvider,CSSReset } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import Footer from "../components/Footer";
 import FormContact from "../components/FormContact";
 import NavBar from "../components/NavBar";
 import Projects from "../components/Projects";
 import Welcome from "../components/Welcome";
 import theme from "../styles/theme";
+import { LanguageProvider } from "../contexts/LanguageProvider";
 
 function App() {
   return (
-    <ChakraProvider theme={theme} >
-      <CSSReset/>
-      <NavBar/>
-      <Welcome/>
-      <Projects/>
-      <FormContact/>
-      <Footer/>
+    <ChakraProvider theme={theme}>
+      <LanguageProvider>
+        <CSSReset />
+        <NavBar />
+        <Welcome />
+        <Projects />
+        <FormContact />
+        <Footer />
+      </LanguageProvider>
     </ChakraProvider>
   );
 }
